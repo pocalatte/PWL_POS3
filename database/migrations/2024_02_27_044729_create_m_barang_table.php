81a2c5c51cf359c75ab8c5f1acd86d31cf5f1761
pocalatte <dittsyn160@gmail.com>
2024-04-16 10:02:01 +0700

    <pindah repository>

--- database/migrations/2024_02_27_044729_create_m_barang_table.php
+++ database/migrations/2024_02_27_044729_create_m_barang_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('m_barang', function (Blueprint $table) {
